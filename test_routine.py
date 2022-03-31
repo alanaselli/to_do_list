071cfd1315e6406c447d4734e875e94e5899572b
Alana Selli <alana.selli@usp.br>
2022-03-31 09:21:54 -0300

    removed useless lines of code

--- test_routine.py
+++ test_routine.py
@@ -1,4 +1,3 @@
-from ast import Continue
 import main
 import os
 
@@ -37,7 +36,6 @@ list_of_tests = [
 list_of_errors = {}
 for i in list_of_tests:
     print("\033[1m" + "\nTest_" + str(list_of_tests.index(i)+1) + ": "  + "\033[0m" + i)
-    # print("Test_" + str(list_of_tests.index(i)+1) + " ran with exit code %d" % os.system(i))
     er = '%d' % os.system(i)
     if er != '0':
         list_of_errors[list_of_tests.index(i)+1] = er
@@ -50,27 +48,3 @@ else:
 print("\nEmptying all lists…")
 os.system("python3.8 main.py -empty")
 print("\nTest routine completed!\n")
-
-# print("\033[1m" + "\ntest_2: "  + "\033[0m" + "python3.8 main.py -d 2022-03-30")
-# test_2 = os.system("python3.8 main.py -d 2022-03-30")
-# print("`test_2` ran with exit code %d" % test_2)
-
-# print("\033[1m" + "\ntest_3: "  + "\033[0m" + "python3.8 main.py -t every_tuesday -wd Tuesday")
-# test_3 = os.system("python3.8 main.py -t every_tuesday -wd Tuesday")
-# print("`test_3` ran with exit code %d" % test_3)
-
-# print("\033[1m" + "\ntest_4: "  + "\033[0m" + "python3.8 main.py -t every_wed_fri -wd Wednesday Friday")
-# test_4 = os.system("python3.8 main.py -t every_wed_fri -wd Wednesday Friday")
-# print("`test_4` ran with exit code %d" % test_4)
-
-# print("\033[1m" + "\ntest_5: "  + "\033[0m" + "python3.8 main.py -d 2022-03-29")
-# test_5 = os.system("python3.8 main.py -d 2022-03-29")
-# print("`test_5` ran with exit code %d" % test_5)
-
-# print("\033[1m" + "\ntest_6: "  + "\033[0m" + "python3.8 main.py -d 2022-03-30")
-# test_6 = os.system("python3.8 main.py -d 2022-03-30")
-# print("`test_6` ran with exit code %d" % test_6)
-
-# print("\033[1m" + "\ntest_7: "  + "\033[0m" + "python3.8 main.py -d 2022-04-01")
-# test_7 = os.system("python3.8 main.py -d 2022-04-01")
-# print("`test_7` ran with exit code %d" % test_7)
